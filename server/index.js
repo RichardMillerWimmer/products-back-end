d43e2f73fc63e46f3883ccd0608f5b3864b34950
Richard Miller <richardmmill@gmail.com>
2021-03-20 14:38:03 -0400

    work on Lab 5.3

---
+++ server/index.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+const massive = require('massive');
+
+const app = express();
+
+
+const { SERVER_PORT, CONNECTION_STRING } = process.env;
+
+app.use(express.json());
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {
+        rejectUnauthorized: false
+    }
+}).then(dbInstance => {
+    app.set('db', dbInstance)
+
+}).catch(error => console.log(error));
+
+
+
+
+
+
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Server listening on port ${SERVER_PORT}`)
+});
